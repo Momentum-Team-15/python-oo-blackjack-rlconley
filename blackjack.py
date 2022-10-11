56c3f4aef8348069ec0a6318f1331b1703feb415
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2022-10-11 15:18:32 -0400

    create card and deck classes

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,71 @@
 # Write your blackjack game here.
+import random
+# Outline my classes
+SUITS = ['♥️', '♣️', '♦️','♠️']
+RANK_VALUES = {
+    'K': 10,
+    'Q': 10,
+    'J': 10,
+    'A': (11, 1),
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9, 
+    10: 10,
+}
+
+
+class Game:
+    pass
+
+
+class Card:
+    def __init__(self, suit, rank, value):
+        self.suit = suit
+        self.rank = rank
+        self.value = value
+
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
+
+# example of building one card
+queen_of_hearts = Card('♥️', 'Q', 10)
+# print(f'{queen_of_hearts} is worth {queen_of_hearts.value}')
+# build a whole deck
+
+
+class Deck:
+    def __init__(self, brand):
+        self.cards = []
+        self.brand = brand
+        self.used = False
+        for suit in SUITS:
+            for rank_value in RANK_VALUES.items():
+                new_card = Card(suit, rank_value[0], rank_value[1])
+                self.cards.append(new_card)
+
+    def __str__(self):
+        return f'{self.brand} deck of {len(self.cards)} cards'
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+# make a new deck of cards, which calls __init__() method
+deck = Deck('Bicycle')
+deck.shuffle()
+for card in deck.cards:
+    print(card)
+
+
+class Player:
+    def __init__(self):
+        self.hand = []
+
+
+class Dealer:
+    def __init__(self):
+        self.hand = []
